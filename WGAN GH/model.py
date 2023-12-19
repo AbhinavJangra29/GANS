096efe3d7ad8a869ebb8accea8c78657bf72b268
Abhinav Jangra <107471490+AbhinavJangra29@users.noreply.github.com>
2023-12-20 03:46:08 +0530

    Update model.py

--- WGAN GH/model.py
+++ WGAN GH/model.py
@@ -1,14 +1,11 @@
-
-import torch
-import torch.nn as nn
-
-
 class Discriminator(nn.Module):
     def __init__(self, channels_img, features_d):
         super(Discriminator, self).__init__()
         self.disc = nn.Sequential(
             # input: N x channels_img x 64 x 64
-            nn.Conv2d(channels_img, features_d, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(
+                channels_img, features_d, kernel_size=4, stride=2, padding=1
+            ),
             nn.LeakyReLU(0.2),
             # _block(in_channels, out_channels, kernel_size, stride, padding)
             self._block(features_d, features_d * 2, 4, 2, 1),
@@ -16,7 +13,6 @@ class Discriminator(nn.Module):
             self._block(features_d * 4, features_d * 8, 4, 2, 1),
             # After all _block img output is 4x4 (Conv2d below makes into 1x1)
             nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=2, padding=0),
-            nn.Sigmoid(),
         )
 
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
@@ -29,7 +25,7 @@ class Discriminator(nn.Module):
                 padding,
                 bias=False,
             ),
-            # nn.BatchNorm2d(out_channels),
+            nn.InstanceNorm2d(out_channels, affine=True),
             nn.LeakyReLU(0.2),
         )
 
@@ -63,7 +59,7 @@ class Generator(nn.Module):
                 padding,
                 bias=False,
             ),
-            # nn.BatchNorm2d(out_channels),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(),
         )
 
@@ -88,7 +84,6 @@ def test():
     z = torch.randn((N, noise_dim, 1, 1))
     assert gen(z).shape == (N, in_channels, H, W), "Generator test failed"
     print("Success, tests passed!")
-
 
 if __name__ == "__main__":
     test()
